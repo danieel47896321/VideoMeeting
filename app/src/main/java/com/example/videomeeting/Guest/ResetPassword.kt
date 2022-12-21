408b2d32a077d8656136147771e9cbc38c80ae43
Daniel <danieel47896321@gmail.com>
2022-12-21 23:58:55 +0200

    Finish Main activity ky and xml. created create account and sign in fragment. created reset password.

---
+++ app/src/main/java/com/example/videomeeting/Guest/ResetPassword.kt
@@ -0,0 +1,12 @@
+package com.example.videomeeting.Guest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.videomeeting.R
+
+class ResetPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
