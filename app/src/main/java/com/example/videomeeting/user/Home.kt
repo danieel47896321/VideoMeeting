e02efe0e5f65d7987ecec293ec9421d392ec3de7
Daniel <danieel47896321@gmail.com>
2022-12-22 13:33:35 +0200

    finish reset password kt. created Home page to go after sign in.

--- app/src/main/java/com/example/videomeeting/user/Home.kt
+++ app/src/main/java/com/example/videomeeting/user/Home.kt
@@ -1,10 +1,11 @@
 package com.example.videomeeting.user
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.videomeeting.R
 
 class Home : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
